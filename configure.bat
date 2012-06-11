0b9d1e3e10f2c4e743150d087fa1cabb6ab04ff5
jazzwang <jazzwang@93027196-e06e-408b-8539-b5ef0f54f2f3>
2012-06-11 08:31:36 +0000

    * modified my_packages/hadoop/bin/hadoop4win-init    - add conf-full and its related configuration files    - link /cygdrive/${DRIVE}/var/hadoop to /var/hadoop  * modified configure.bat    - fixed path of cyg-setup.exe
    git-svn-id: https://hadoop4win.svn.sf.net/svnroot/hadoop4win@58 93027196-e06e-408b-8539-b5ef0f54f2f3

--- configure.bat
+++ configure.bat
@@ -1,4 +1,4 @@
 REM 純下載 Cygwin Base 分類下的檔案
-cyg-setup.exe -q -D -O -s http://mirror.mcs.anl.gov/cygwin -C base -P cygrunsrv,file,openssh,perl,procps,ncurses,rsync,sharutils,shutdown,subversion,tcp_wrappers,termcap,unzip,wget,zip,zlib,wget
+files\cyg-setup.exe -q -D -O -s http://mirror.mcs.anl.gov/cygwin -C base -P cygrunsrv,file,openssh,perl,procps,ncurses,rsync,sharutils,shutdown,subversion,tcp_wrappers,termcap,unzip,wget,zip,zlib,wget
 REM 把 http 開頭的目錄搬到 cygwin_mirror
 move http* cygwin_mirror
