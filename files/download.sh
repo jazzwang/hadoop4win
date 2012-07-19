7a6b080e754869187f2125fe594ad9ee2da04c85
Jazz Yao-Tsung Wang <jazzwang.tw@gmail.com>
2012-07-19 05:55:56 +0000

    * modified hadoop4win-full.nsi    - change from hadoop 1.0.3 back to hadoop 0.20.2      since 1.0.3 could not chmod ${hadoop.tmp.dir}/mapred/  * modified my_packages/hadoop/bin/hadoop4win-init    - force to have cygfile:/var in ${INSTDIR} of hadoop4win      Ex. link c:/hadoop4win/var to /var    - add back $HADOOP_HOME for hadoop 0.20.2  * add files/download.sh    - for downloading package files for hadoop4win-full.nsi
    git-svn-id: svn+ssh://svn.code.sf.net/p/hadoop4win/code@59 93027196-e06e-408b-8539-b5ef0f54f2f3

---
+++ files/download.sh
@@ -0,0 +1,6 @@
+wget http://cygwin.com/setup.exe -O cyg-setup.exe
+wget http://ftp.twaren.net/Unix/Web/apache/ant/binaries/apache-ant-1.8.4-bin.tar.gz -O ant-1.8.4-bin.tar.gz
+wget http://ftp.twaren.net/Unix/Web/apache/hbase/stable/hbase-0.92.1.tar.gz
+wget http://ftp.twaren.net/Unix/Web/apache/hadoop/common/hadoop-0.20.2/hadoop-0.20.2.tar.gz
+wget http://ftp.twaren.net/Unix/Web/apache/pig/stable/pig-0.10.0.tar.gz
+wget http://ftp.twaren.net/Unix/Web/apache/hive/stable/hive-0.8.1-bin.tar.gz
