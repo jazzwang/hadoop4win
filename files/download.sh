371639f70b2d1db5ec3efeab722ff41709cca705
jazzwang <jazzwang@93027196-e06e-408b-8539-b5ef0f54f2f3>
2012-09-13 15:35:53 +0000

    * upstream upload new stable version
    git-svn-id: https://hadoop4win.svn.sf.net/svnroot/hadoop4win@66 93027196-e06e-408b-8539-b5ef0f54f2f3

--- files/download.sh
+++ files/download.sh
@@ -1,7 +1,7 @@
 wget "http://cygwin.com/setup.exe" -O cyg-setup.exe
 wget "http://downloads.sourceforge.net/project/nsis/NSIS%202/2.46/nsis-2.46-setup.exe"
 wget "http://ftp.twaren.net/Unix/Web/apache/ant/binaries/apache-ant-1.8.4-bin.tar.gz" -O ant-1.8.4-bin.tar.gz
-wget "http://ftp.twaren.net/Unix/Web/apache/hbase/stable/hbase-0.94.1.tar.gz"
+wget "http://ftp.twaren.net/Unix/Web/apache/hbase/stable/hbase-0.92.2.tar.gz"
 wget "http://ftp.twaren.net/Unix/Web/apache/hadoop/common/stable/hadoop-1.0.3.tar.gz"
 wget "http://ftp.twaren.net/Unix/Web/apache/pig/stable/pig-0.10.0.tar.gz"
 wget "http://ftp.twaren.net/Unix/Web/apache/hive/stable/hive-0.8.1-bin.tar.gz"
