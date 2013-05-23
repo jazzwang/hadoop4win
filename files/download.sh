d4d5d8cc863f686defb55cc587a01b91fe776993
Jazz Wang <jazzwang.tw@gmail.com>
2013-05-24 00:18:20 +0800

    * upgrade ant from 1.9.0 to 1.9.1

--- files/download.sh
+++ files/download.sh
@@ -1,6 +1,6 @@
 wget "http://cygwin.com/setup.exe"							-O cyg-setup.exe
 wget "http://downloads.sourceforge.net/project/nsis/NSIS%202/2.46/nsis-2.46-setup.exe"	-O nsis-2.46-setup.exe
-wget "http://ftp.twaren.net/Unix/Web/apache/ant/binaries/apache-ant-1.9.0-bin.tar.gz"	-O ant-1.9.0-bin.tar.gz
+wget "http://ftp.twaren.net/Unix/Web/apache/ant/binaries/apache-ant-1.9.1-bin.tar.gz"	-O ant-1.9.1-bin.tar.gz
 wget "http://ftp.twaren.net/Unix/Web/apache/hbase/stable/hbase-0.94.7.tar.gz"		-O hbase-0.94.7.tar.gz
 wget "http://ftp.twaren.net/Unix/Web/apache/hadoop/common/stable/hadoop-1.0.4.tar.gz"	-O hadoop-1.0.4.tar.gz
 wget "http://ftp.twaren.net/Unix/Web/apache/pig/stable/pig-0.10.1.tar.gz"		-O pig-0.10.1.tar.gz
