3cdf5a2e004bae596cf8ebc86ef1aa807be9ac2e
Jazz Wang <jazzwang.tw@gmail.com>
2013-07-01 18:10:27 +0800

    * update download.sh    - upgrade pig to 0.11.1    - upgrade hive to 0.10.0

--- files/download.sh
+++ files/download.sh
@@ -3,5 +3,5 @@ wget "http://downloads.sourceforge.net/project/nsis/NSIS%202/2.46/nsis-2.46-setu
 wget "http://ftp.twaren.net/Unix/Web/apache/ant/binaries/apache-ant-1.9.1-bin.tar.gz"	-O ant-1.9.1-bin.tar.gz
 wget "http://ftp.twaren.net/Unix/Web/apache/hbase/stable/hbase-0.94.8.tar.gz"		-O hbase-0.94.8.tar.gz
 wget "http://ftp.twaren.net/Unix/Web/apache/hadoop/common/stable/hadoop-1.1.2.tar.gz"	-O hadoop-1.1.2.tar.gz
-wget "http://ftp.twaren.net/Unix/Web/apache/pig/stable/pig-0.10.1.tar.gz"		-O pig-0.10.1.tar.gz
-wget "http://ftp.twaren.net/Unix/Web/apache/hive/stable/hive-0.9.0-bin.tar.gz"		-O hive-0.9.0-bin.tar.gz
+wget "http://ftp.twaren.net/Unix/Web/apache/pig/stable/pig-0.11.1.tar.gz"		-O pig-0.11.1.tar.gz
+wget "http://ftp.twaren.net/Unix/Web/apache/hive/stable/hive-0.10.0-bin.tar.gz"		-O hive-0.10.0-bin.tar.gz
